3a4de6b2dfc97760c8c277813816e531102821d2
Darren <darren277@yahoo.com>
2022-12-14 09:33:34 -0800

    Refactored webhooks file to be more readable and start using string formatting.

--- webhooks/webhooks.py
+++ webhooks/webhooks.py
@@ -4,13 +4,57 @@ from webhooks.factories import issueWebhookFactory
 from webhooks.utils import SlackCommentTemplate
 
 openIssueWebhook = issueWebhookFactory.createWebhook('open',
-    slack_comment_template = SlackCommentTemplate(*[{"type": "section", "text": {"type": "mrkdwn", "text": "A new issue has been *created*: <"+GITHUB_REPO+"issues/" + '{{issue.number}}' + "|#" + '{{issue.number}}' + "> | Labels: " + '{% for label in issue.labels %}{{ label.name }}{{ ", " if not loop.last else "" }}{% endfor %}'}}, {"type": "section", "text": {"type": "mrkdwn", "text": '{{ issue.title }}'}}])
+    slack_comment_template = SlackCommentTemplate(*[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"A new issue has been *created*: <{GITHUB_REPO}issues/" + '{{issue.number}}' + "|#" + '{{issue.number}}' + "> | Labels: " + '{% for label in issue.labels %}{{ label.name }}{{ ", " if not loop.last else "" }}{% endfor %}'
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": '{{ issue.title }}'
+            }
+        }
+    ])
 )
 
 closeIssueWebhook = issueWebhookFactory.createWebhook('close',
-    slack_comment_template = SlackCommentTemplate(*[{"type": "section", "text": {"type": "mrkdwn", "text": "An issue has been *closed*: <"+GITHUB_REPO+"issues/" + '{{issue.number}}' + "|#" + '{{issue.number}}' + "> | Labels: " + '{% for label in issue.labels %}{{ label.name }}{{ ", " if not loop.last else "" }}{% endfor %}'}}, {"type": "section", "text": {"type": "mrkdwn", "text": '{{ issue.title }}'}}])
+    slack_comment_template = SlackCommentTemplate(*[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"An issue has been *closed*: <{GITHUB_REPO}issues/" + '{{issue.number}}' + "|#" + '{{issue.number}}' + "> | Labels: " + '{% for label in issue.labels %}{{ label.name }}{{ ", " if not loop.last else "" }}{% endfor %}'
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn", "text": '{{ issue.title }}'
+            }
+        }
+    ])
 )
 
 reopenIssueWebhook = issueWebhookFactory.createWebhook('reopen',
-slack_comment_template = SlackCommentTemplate(*[{"type": "section", "text": {"type": "mrkdwn", "text": "An issue has been *reopened*: <"+GITHUB_REPO+"issues/" + '{{issue.number}}' + "|#" + '{{issue.number}}' + "> | Labels: " + '{% for label in issue.labels %}{{ label.name }}{{ ", " if not loop.last else "" }}{% endfor %}'}}, {"type": "section", "text": {"type": "mrkdwn", "text": '{{ issue.title }}'}}])
+    slack_comment_template = SlackCommentTemplate(*[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"An issue has been *reopened*: <{GITHUB_REPO}issues/" + '{{issue.number}}' + "|#" + '{{issue.number}}' + "> | Labels: " + '{% for label in issue.labels %}{{ label.name }}{{ ", " if not loop.last else "" }}{% endfor %}'
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": '{{ issue.title }}'
+            }
+        }
+    ])
 )
