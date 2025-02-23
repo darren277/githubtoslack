4527c13b4d62d7d2f4203b4606d8c6d29c0312f7
darren277 <darren277@yahoo.com>
2025-02-23 08:39:55 -0800

    Removed unnecessary dict call.

--- webhooks/interfaces.py
+++ webhooks/interfaces.py
@@ -43,11 +43,11 @@ class SGWebhook(Webhook):
     # slack_relay_endpoint: str = '/sg'
 
     def post(self):
-        d = self.slack_comment_template.format(dict(
+        d = self.slack_comment_template.format(
             event_type=self.event_type,
             email=self.email,
             reason=self.reason
-        ))
+        )
         req = requests.post(self.slack_relay_endpoint, headers={'Content-Type': 'application/json'}, data=json.dumps(dict(blocks=d)))
         return req.status_code
 
